8f0869a4086f4cbc5911d09dc6c56f51464b9307
C315 <102221577+C315@users.noreply.github.com>
2022-09-18 19:55:06 +0900

    Add files via upload

---
+++ Python_Studying/02_turtle.py
@@ -0,0 +1,14 @@
+#2. turtle
+import turtle as t
+t.shape('turtle')
+t.shapesize(3)
+
+for i in range(5):
+    t.fd(200)
+    t.left(72)
+'''
+t.shape("") #거북이 모양 변화: turtle, circle, square 등
+t.shapesize(숫자) #거북이 크기 변화
+t.color("") #거북이 색 변화: b, r, g, yellow 등
+t.방향(거리) #거북이 이동
+'''
